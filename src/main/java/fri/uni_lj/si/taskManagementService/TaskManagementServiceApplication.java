1f655edee2365807de74500a2b2119d523b3da1c
zigakleine <ziga.kleine@gmail.com>
2020-10-29 19:08:58 +0100

    Added SpringBoot boilerplate.

---
+++ src/main/java/fri/uni_lj/si/taskManagementService/TaskManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package fri.uni_lj.si.taskManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fri/uni_lj/si/taskManagementService/TaskManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fri.uni_lj.si.taskManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
